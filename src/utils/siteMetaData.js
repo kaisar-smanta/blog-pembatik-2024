4f61ba73b9694896761ed4378a726df971a025fd
Kaisar Titoniran Akbar <59471690+Arkai-Schrodinger@users.noreply.github.com>
2024-10-27 23:29:00 +0800

    Slightly Refining The data

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,21 +1,17 @@
 // please update required information
 const siteMetadata = {
-    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
+    title: 'Website Blog Pembatik 2024',
     author: 'CodeBucks',
-    headerTitle: 'Next.js Blog',
-    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+    headerTitle: 'Blog Pembatik 2024',
+    description: 'Blog Artikel Pembelajaran Berbasis TIK Tahun 2024',
     language: 'en-us',
     theme: 'system', // system, dark or light
-    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteUrl: 'https://pembatik.titoniran.com/', // your website URL
     siteLogo: '/logo.png',
     socialBanner: '/social-banner.png', // add social banner in the public folder
-    email: 'codebucks27@gmail.com', 
-    github: 'https://github.com/codebucks27',
-    twitter: 'https://twitter.com/code_bucks',
-    facebook: 'https://facebook.com',
-    youtube: 'https://youtube.com/codebucks',
-    linkedin: 'https://www.linkedin.com/in/codebucks/',
-    dribbble: 'https://www.dribbble.com',
+    email: 'kaisar591@guru.sma.belajar.id', 
+    github: 'https://github.com/kaisar-smanta',
+    youtube: 'https://www.youtube.com/@kaisarbelajarid',
     locale: 'en-US',
   }
   
