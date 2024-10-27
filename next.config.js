0d07b5eda025daecc2f980519e40e3b3c815560c
Kaisar Titoniran Akbar <59471690+Arkai-Schrodinger@users.noreply.github.com>
2024-10-27 17:16:51 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
-/** @type {import('next').NextConfig} */
+const { withContentlayer } = require("next-contentlayer")
+
 const nextConfig = {
     compiler: {
         removeConsole: true,
     },
     output: "export",
     reactStrictMode: true,
-    basePath: process.env.NODE_ENV === "production" ? "/blog-pembatik-2024" : "",
+    basePath: process.env.NODE_ENV === "production" ? "/blog-pembatik-2024" : "/",
     images: {
         unoptimized: true,
     },
 };
 
-module.exports = nextConfig;
+module.exports = withContentlayer({ ...nextConfig });
